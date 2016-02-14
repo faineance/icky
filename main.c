f97f702726527904d393b73481dfeb954d307aea
Anthony <faineance@users.noreply.github.com>
2016-02-14 19:23:34 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main() {
     uint8_t hello[] = {
             PUSH, 0x00, 0x01,
             PUSH, 0x00, 0x02,
+            IADD
     };
     VM *vm = vm_create(hello, sizeof(hello));
     vm_run(vm);
